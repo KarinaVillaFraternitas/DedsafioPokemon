aad338a1c69db690e5b6b4499a9479dc053745e4
KarinaVillaFraternitas <karina.villa@fraternitas.io>
2024-05-08 11:42:10 -0600

    Cambios nuevos

--- js/comodin.js
+++ js/comodin.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.getElementById('pricingTablesContainer');
-    for (let i = 0; i <= 21; i++) {
-        const imageNumber = i.toString().padStart(3, '0');
-        const imagePath = `img/comodines/${imageNumber}.png`;
-        const colClass = i > 2 ? 'col-lg-3 col-md-3 col-xs-12 mt-1 mb-1' : 'col-lg-3 col-md-3 col-xs-12';
 
-        const colElement = `
-            <div class="${colClass}">
-                <div class="pricing-table text-center">
-                    <div class="pricing-details">
-                        <img src="${imagePath}" alt="">
-                    </div>
-                </div>
-            </div>
-        `;
+    fetch('public/src/images.json')
+        .then(response => response.json())
+        .then(data => {
+            data.images.forEach((item, index) => {
+                const colDiv = document.createElement('div');
+                const pricingTable = document.createElement('div');
+                const pricingDetails = document.createElement('div');
+                const link = document.createElement('a'); // Crea un elemento de enlace
+                const img = document.createElement('img');
 
-        container.innerHTML += colElement;
-    }
+                const colClass = index > 2 ? 'col-lg-3 col-md-3 col-xs-12 mt-1 mb-1' : 'col-lg-3 col-md-3 col-xs-12';
+                colDiv.className = colClass;
+                pricingTable.className = 'pricing-table text-center';
+                pricingDetails.className = 'pricing-details';
+
+                link.href = item.src; // URL de la imagen de alta resolución
+                link.setAttribute('data-lightbox', 'image-gallery');
+                link.setAttribute('data-title', item.description);
+
+                img.src = item.src; // URL de la miniatura
+                img.alt = item.description;
+                img.title = item.description;
+
+                link.appendChild(img); // Añade la imagen al enlace
+                pricingDetails.appendChild(link); // Añade el enlace a pricingDetails
+                pricingTable.appendChild(pricingDetails);
+                colDiv.appendChild(pricingTable);
+                container.appendChild(colDiv);
+            });
+        })
+        .catch(error => console.error('Error loading the images:', error));
 });
